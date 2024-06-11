05640281e60104e7c94b89f268d5cd06845728ce
Vasil Chimev <vasil.chimev@gmail.com>
2024-06-11 16:45:56 +0300

    Update max options count

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 export const ALPHA_AZ_VALUE = "alphaAZ";
-export const MAX_OPTIONS_COUNT = 1; // Demo only
+export const MAX_OPTIONS_COUNT = 5; // Demo only
 export const MOCKY_URL = "https://run.mocky.io/v3/a6d770f7-b23f-4839-9abe-d32f1f06be10";
 export const NEWLINE_CHARACTER = "\n";
 export const NON_BREAKING_SPACE = "&nbsp;";
